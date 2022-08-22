bf17b4e281122bdbce7fc0f73d2edada77f310bb
Nguyen Hoang Linh <linh072217@gmail.com>
2022-08-22 07:23:33 +0700

    feat: setup project

---
+++ src/main/java/com/hoanglinhplus/CareerSocialNetwork/CareerSocialNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.hoanglinhplus.CareerSocialNetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerSocialNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerSocialNetworkApplication.class, args);
+	}
+
+}
